97f8912abcab5b764cc0ce4a87e91047cbe91066
Ken <ccw@alltrue.ai>
2025-03-18 20:05:23 -0700

    Added Github release actions (#12)

--- src/build.py
+++ src/build.py
@@ -27,22 +27,26 @@ def pdm_build_update_files(context, files):
     context.config.metadata["optional-dependencies"] = dict(
         filter(lambda dep: "observers" in dep[0], optionals.items())
     )
-    with open(
-        os.path.join(_CORE_HOME, "..", "pyproject.toml"), "rb"
-    ) as core_project_file:
-        core_project = tomllib.load(core_project_file)
-        if not "dependencies" in context.config.metadata:
-            context.config.metadata["dependencies"] = []
-        context.config.metadata["dependencies"] = list(
-            filter(
-                lambda dep: "alltrue-llm-observability-core" not in dep,
-                context.config.metadata.pop("dependencies", []),
+
+    core_project = os.path.join(_CORE_HOME, "..", "pyproject.toml")
+    if os.path.exists(core_project):
+        with open(core_project, "rb") as core_project_file:
+            core_project_config = tomllib.load(core_project_file)
+            if not "dependencies" in context.config.metadata:
+                context.config.metadata["dependencies"] = []
+            context.config.metadata["dependencies"] = list(
+                filter(
+                    lambda dep: "alltrue-llm-observability-core" not in dep,
+                    context.config.metadata.pop("dependencies", []),
+                )
             )
+            context.config.metadata["dependencies"].extend(
+                core_project_config["project"]["dependencies"]
+            )
+        src_prefix = (
+            (_CORE_HOME + os.path.sep)
+            if context.target == "wheel"
+            else ("core" + os.path.sep)
         )
-        context.config.metadata["dependencies"].extend(
-            core_project["project"]["dependencies"]
-        )
-    for src in glob.glob(os.path.join(_CORE_HOME, "**", "*.py"), recursive=True):
-        files[src.removeprefix(_CORE_HOME + os.path.sep)] = pathlib.Path(
-            os.path.abspath(src)
-        )
+        for src in glob.glob(os.path.join(_CORE_HOME, "**", "*.py"), recursive=True):
+            files[src.removeprefix(src_prefix)] = pathlib.Path(os.path.abspath(src))
